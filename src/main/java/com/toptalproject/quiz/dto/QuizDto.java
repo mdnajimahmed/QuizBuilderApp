9835ca76499f380b08c0e9245d3528060bc093d2
Md Najim Ahmed <najim.ju@gmail.com>
2023-04-23 18:16:02 +0800

    progress

--- src/main/java/com/toptalproject/quiz/dto/QuizDto.java
+++ src/main/java/com/toptalproject/quiz/dto/QuizDto.java
@@ -11,7 +11,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 import org.hibernate.validator.constraints.Length;
 
 @Data
