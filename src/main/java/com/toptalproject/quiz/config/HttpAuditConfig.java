5590670932f3c767114308c6b859321c97e103ae
Md Najim Ahmed <najim.ju@gmail.com>
2023-09-10 21:19:35 +0800

    add buildspec.yml

---
+++ src/main/java/com/toptalproject/quiz/config/HttpAuditConfig.java
@@ -0,0 +1,15 @@
+package com.toptalproject.quiz.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class HttpAuditConfig {
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        final CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        return loggingFilter;
+    }
+}
