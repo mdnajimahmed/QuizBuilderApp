dd4e46af8f7dfaa482b8cea5d947243809f9bdc3
Md Najim Ahmed <najim.ju@gmail.com>
2024-01-27 11:39:06 +0800

    test enhancement

--- src/test/java/com/toptalproject/quiz/controller/QuizControllerTest.java
+++ src/test/java/com/toptalproject/quiz/controller/QuizControllerTest.java
@@ -1,6 +1,7 @@
 package com.toptalproject.quiz.controller;
 
 import com.toptalproject.quiz.ApplicationNoSecurity;
+import com.toptalproject.quiz.ControllerAdvisor;
 import com.toptalproject.quiz.PrintRequestAndResponseResultHandler;
 import com.toptalproject.quiz.config.HttpAuditConfig;
 import com.toptalproject.quiz.dto.QuizDto;
@@ -22,11 +23,14 @@ import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
 @WebMvcTest(value = QuizController.class)
-@ContextConfiguration(classes = {QuizController.class, WavefrontProperties.Application.class, ApplicationNoSecurity.class,
-        HttpAuditConfig.class})
+@ContextConfiguration(classes = {
+        QuizController.class, WavefrontProperties.Application.class, ApplicationNoSecurity.class,
+        HttpAuditConfig.class, ControllerAdvisor.class
+})
 class QuizControllerTest {
     @Autowired
     private WebApplicationContext context;
@@ -50,7 +54,11 @@ class QuizControllerTest {
                 .content(mapper.writeValueAsBytes(dto))
         );
         response.andExpect(MockMvcResultMatchers.status().isBadRequest())
-                .andDo(PrintRequestAndResponseResultHandler.printRequestAndResponse());
+                .andDo(PrintRequestAndResponseResultHandler.printRequestAndResponse())
+                .andExpect(jsonPath("$.violations").exists())
+                .andExpect(jsonPath("$.violations[0].fieldName").value("title"))
+                .andExpect(jsonPath("$.violations[0].message").value("quiz title can not be empty"));
+
 
     }
 }
